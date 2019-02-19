d83db3dddbb4355fb0a4d7fd52f48e0c6f9a5e7f
Ahmed Elzarzamony <ahmed.mrcode@gmail.com>
2019-02-19 23:39:09 +0200

    change interface methods

--- lib/FormatInterface.php
+++ lib/FormatInterface.php
@@ -4,26 +4,8 @@ namespace Importer\Lib;
 
 interface FormatInterface{
 
-    /**
-     * getType function
-     ** check the given string if it [file] or [api url]
-     * @param string $str
-     * @return Str
-     */
-    public function getType(string $str);
-
-    /**
-     * getFormat function
-     ** return the real format for given strucure
-     * @return Str
-     */
-    public function getFormat();
-
-    /**
-     * ConvertToArray function
-     ** convert given strucure from xml,json or other into array, we doing it for database insertion
-     * @return Array
-     */
+    public function isFormat();
+    public function getContent();
     public function ConvertToArray();
 
 }
